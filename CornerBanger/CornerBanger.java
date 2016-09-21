ef3a7c469314425fbc61502e0af6d604981fb6c6
Joshua Kelley <joshuakelley@Joshua-ZCW-17.local>
2016-09-21 18:04:58 -0400

    first robot

---
+++ CornerBanger/CornerBanger.java
@@ -0,0 +1,62 @@
+package CornerBanger;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
+
+/**
+ * CornerBanger - a robot by (your name here)
+ */
+public class CornerBanger extends AdvancedRobot
+{
+	/**
+	 * run: CornerBanger's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			if(getEnergy()<50){
+				
+			}
+			ahead(500);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		
+		turnRight(e.getBearing());
+		fire(50);
+		ahead(e.getDistance());
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
